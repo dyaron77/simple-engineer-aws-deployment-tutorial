ca20e72f8ce124d769a5f38186cd528eb15f5b6a
Dovy <dov.yaron@gmail.com>
2022-05-13 13:00:53 -0400

    Deployment-tutorial1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          New sherif in town
         </p>
         <a
           className="App-link"
